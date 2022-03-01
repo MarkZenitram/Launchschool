ee33bc7c083b236015d03110d8f7ddf6cda4db3f
MarkZenitram <markmartinez.pharmd@gmail.com>
2022-02-28 21:34:04 -0800

    all changes

--- ruby_book_exercises/hash_ex4.rb
+++ ruby_book_exercises/hash_ex4.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 numbers = []
 count = 0
 
@@ -7,3 +8,12 @@ while count < 5
 end
 
 p numbers
+=======
+# Use Ruby's Array method delete_if and String method start_with? to delete all of the strings that begin with an "s" in the following array.
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+puts arr.delete_if { |v| v.start_with?("s") }
+
+
+>>>>>>> a2669d2 (all changes)

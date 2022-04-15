f88798badd7216b7ccfde3110364d4902035d611
MarkZenitram <markmartinez.pharmd@gmail.com>
2022-04-14 21:58:54 -0700

    Redid hard problem in lesson 3

--- rb101/lesson_3/hard_problems.rb
+++ rb101/lesson_3/hard_problems.rb
@@ -1,8 +1,15 @@
 def dot_separated_ip_address?(input_string)
-  dot_separated_words = input_string.split(".")
+  p dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words == 4
+
   while dot_separated_words.size > 0 do
     word = dot_separated_words.pop
-    break unless is_an_ip_number?(word)
+    return false if is_an_ip_number?(word) == false
   end
   return true
 end
+
+def is_an_ip_number?(string_number)
+  string_number.to_i >= 0 && string_number.to_i <= 255
+end
+

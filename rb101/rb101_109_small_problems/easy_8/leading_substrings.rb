6b739919ea79330e80ac42d98e98bfd0ec18b3b1
MarkZenitram <markmartinez.pharmd@gmail.com>
2022-05-14 11:31:34 -0700

    More easy 8 problems

---
+++ rb101/rb101_109_small_problems/easy_8/leading_substrings.rb
@@ -0,0 +1,17 @@
+def leading_substrings(string)
+  index = 1
+  substring_array = []
+  substring_array << string[0]
+
+  loop do
+    break if substring_array.size == string.size
+    substring_array << (substring_array[index - 1] + string[index])
+    index += 1
+  end
+  p substring_array
+end
+
+
+#leading_substrings('abc') == ['a', 'ab', 'abc']
+#leading_substrings('a') == ['a']
+p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']

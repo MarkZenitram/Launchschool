3d4665d46dcfea30e334a21186e8791d1b6633e4
MarkZenitram <markmartinez.pharmd@gmail.com>
2022-06-24 13:32:16 -0700

    Completed tic tac toe from lesson 6

--- Ruby_Test/example.rb
+++ Ruby_Test/example.rb
@@ -1,5 +1,14 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-for char in alphabet.chars
-  puts char
+def joinor(array, delimiter=', ', word='or')
+  if array.size <= 2
+    array.join(" #{word} ")
+  else
+    last_element = array.pop
+    string_of_array = array.join("#{delimiter}")
+    new_string = string_of_array + "#{delimiter}" + word + ' ' + last_element.to_s
+  end
 end
+
+p joinor([1, 2])                   # => "1 or 2"
+p joinor([1, 2, 3])                # => "1, 2, or 3"
+p joinor([1, 2, 3], '; ')          # => "1; 2; or 3"
+p joinor([1, 2, 3], ', ', 'and')   # => "1, 2, and 3"

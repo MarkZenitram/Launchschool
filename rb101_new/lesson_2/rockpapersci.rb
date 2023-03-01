032cef21d0ab338f67fbc6d928c57e1357376554
MarkZenitram <markmartinez.pharmd@gmail.com>
2023-02-28 16:48:23 -0800

    Completed rock paper scissors

--- rb101_new/lesson_2/rockpapersci.rb
+++ rb101_new/lesson_2/rockpapersci.rb
@@ -1,22 +1,49 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w[rock paper scissors]
 
 def prompt(msg)
   puts("=> #{msg}")
 end
 
-choice = ''
-loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = gets.chomp
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'paper' && second == 'rock') ||
+  (first == 'scissors' && second == 'paper')
+end
 
-  if VALID_CHOICES.include?(choice)
-    break
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt('You win!')
+  elsif win?(computer, player)
+    prompt('Computer wins!')
   else
-    prompt("Not a valid choice")
+    prompt('Tie!')
   end
 end
 
-computer_choice = VALID_CHOICES.sample
+choice = ''
 
-prompt("You choice: #{choice} | Computer choice: #{computer_choice}")
+loop do
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
 
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("Not a valid choice")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("Your choice: #{choice} | Computer choice: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Would you like to play again?")
+  play_again = gets.chomp
+
+  break unless play_again.start_with?('y')
+end
+
+prompt("Thanks for playing")
